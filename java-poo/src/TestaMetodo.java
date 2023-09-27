98a4a9d3aa47d882f1a2c921aa5dbaa06ea2b49c
Thuany Mendonca <thuany1997@hotmail.com>
2023-09-27 10:51:19 -0300

    Trabalhando com metodos

---
+++ java-poo/src/TestaMetodo.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class TestaMetodo {
+    public static void main(String[] args) {
+        Conta contaDoPaulo = new Conta();
+        contaDoPaulo.saldo = 50;
+        contaDoPaulo.deposita(100);
+        System.out.println(contaDoPaulo.saldo);
+
+        boolean conseguiuRetirar = contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conseguiuRetirar);
+
+        Conta contaDaMarcela = new Conta();
+        contaDaMarcela.deposita(1000);
+        contaDaMarcela.transfere(200, contaDoPaulo);
+        System.out.println(contaDaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo);
+
+    }
+}
