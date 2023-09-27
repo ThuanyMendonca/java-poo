9b614778b876720bba499345a1c1d074f524db8a
Thuany Mendonca <thuany1997@hotmail.com>
2023-09-27 13:09:53 -0300

    Metodos get e set

---
+++ bytebank-encapsulamento/src/Conta.java
@@ -0,0 +1,50 @@
+public class Conta {
+    // Encapsulamento: somente a própria classe consegue acessar o atributo private
+    // para outras classes terem acesso, é necessário criar métodos na classe para suprir
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public void deposita(double valor) {
+        saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saldo >= valor) {
+            this.saca(valor);
+            destino.deposita(valor);
+            return true;
+        }
+
+        return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+}
---
+++ bytebank-encapsulamento/src/TestaGetESet.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class TestaGetESet {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+
+        conta.setNumero(1233);
+        System.out.println(conta.getNumero());
+    }
+}
